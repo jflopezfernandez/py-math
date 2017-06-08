13e26662a0cafda521196290a7c632d6b8d75554
lopezfjose <jflopezfernandez@gmail.com>
2017-06-08 02:38:56 -0400

    .

--- src/main.py
+++ src/main.py
@@ -7,60 +7,10 @@ Author: Jose Lopez
 """
 
 # Euclid's Algorithm to determine the Greatest Common Divisor
+import number
 
-def getGCD(m, n):
-	r = m % n;
-
-	while (r != 0):
-		m = n;
-		n = r;
-
-		r = m % n;
-
-	return n;
-
-
-class Fraction:
-	def __init__(self, numerator, denominator):
-		self.numerator = numerator;
-		self.denominator = denominator;
-
-	def __str__(self):
-		self.simplify();
-
-		if (self.denominator == 0):
-			print("[Error] Invalid number!");
-		else:
-			if (self.denominator == 1):
-				return '{self.numerator}'.format(self=self);
-			elif (self.numerator == 0):
-				return '0';
-			else:
-				return '{self.numerator}/{self.denominator}'.format(self=self);
-
-	def __add__(self, other):
-		
-		if (self.denominator == other.denominator):
-			return Fraction(self.numerator + other.numerator, self.denominator);
-		else:
-			result_denominator = self.denominator * other.denominator;
-			result_numerator = (self.numerator * other.denominator) + (self.denominator * other.numerator);
-
-			result = Fraction(result_numerator, result_denominator);
-			result.simplify();
-
-			return result;
-
-	def simplify(self):
-		gcd = getGCD(self.numerator, self.denominator);
-
-		if (gcd > 1):
-			self.numerator = int(self.numerator / gcd);
-			self.denominator = int(self.denominator / gcd);
-
-
-a = Fraction(4,6);
-b = Fraction(6,9);
+a = number.Fraction(4,6);
+b = number.Fraction(6,9);
 
 print(a);
 print(b);
--- src/number.py
+++ src/number.py
@@ -1,9 +1,49 @@
-class Point:
-	def __init__(a, b, c):
-		self.x = a
-		self.y = b
-		self.z = c
+def getGCD(m, n):
+	r = m % n;
 
-	def printPoint():
-		print("({0},{1},{2})", self.x, self.y, self.z)
+	while (r != 0):
+		m = n;
+		n = r;
 
+		r = m % n;
+
+	return n;
+
+
+class Fraction:
+	def __init__(self, numerator, denominator):
+		self.numerator = numerator;
+		self.denominator = denominator;
+
+	def __str__(self):
+		self.simplify();
+
+		if (self.denominator == 0):
+			print("[Error] Invalid number!");
+		else:
+			if (self.denominator == 1):
+				return '{self.numerator}'.format(self=self);
+			elif (self.numerator == 0):
+				return '0';
+			else:
+				return '{self.numerator}/{self.denominator}'.format(self=self);
+
+	def __add__(self, other):
+		
+		if (self.denominator == other.denominator):
+			return Fraction(self.numerator + other.numerator, self.denominator);
+		else:
+			result_denominator = self.denominator * other.denominator;
+			result_numerator = (self.numerator * other.denominator) + (self.denominator * other.numerator);
+
+			result = Fraction(result_numerator, result_denominator);
+			result.simplify();
+
+			return result;
+
+	def simplify(self):
+		gcd = getGCD(self.numerator, self.denominator);
+
+		if (gcd > 1):
+			self.numerator = int(self.numerator / gcd);
+			self.denominator = int(self.denominator / gcd);
